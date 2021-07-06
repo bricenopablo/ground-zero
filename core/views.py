dcba5108a5a6080cbed4a3a529903070594bc6f3
Juan Pablo Navarrete <jpablo.javier.nb@gmail.com>
2021-07-06 13:55:42 -0400

    Traspaso de proyecto a django

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+  return render(request, 'core/home.html')
+
+def login(request):
+  return render(request, 'core/login.html')
+
+def register(request):
+  return render(request, 'core/register.html')
